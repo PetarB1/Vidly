32850669914a892732566fa8076a62f8d66ad27d
Petar <Petar@DESKTOP-N6HE29J>
2023-06-20 16:35:32 +0200

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie { Name = "Shrek" });
+            movies.Add(new Movie { Name = "Harry Potter" });
+
+            return View(movies);
+        }
+    }
+}
